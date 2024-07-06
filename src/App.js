1c051cc76649f4ad2ab1a303808c1a8692174647
Antonio Oliveira <antoniop_neto@hotmail.com>
2024-07-06 17:27:58 +0100

    Accordion V2 - Only one open at the same time

--- src/App.js
+++ src/App.js
@@ -25,31 +25,37 @@ export default function App() {
 }
 
 function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
   return (
     <div className="accordion">
-      {data.map((el, num) => (
+      {data.map((el, i) => (
         <AccordionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
           title={el.title}
-          text={el.text}
-          num={num}
+          num={i}
           key={el.title}
-        />
+        >
+          {el.text}
+        </AccordionItem>
       ))}
     </div>
   );
 }
 
-function AccordionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
+
+  function handleToggle() {
+    onOpen(isOpen ? null : num);
+  }
+
   return (
-    <div
-      onClick={() => setIsOpen(!isOpen)}
-      className={isOpen ? "item open" : "item"}
-    >
+    <div onClick={handleToggle} className={isOpen ? "item open" : "item"}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
